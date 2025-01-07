2c8563ad69e7834a7dd6a71c86c67ea4db9a797e
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2025-01-08 00:47:36 +0300

    feat: show hide password

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -17,6 +17,7 @@ import { AuthService } from '../../auth/auth.service';
 export class LoginPageComponent {
   authService = inject(AuthService);
   router = inject(Router);
+  isPasswordVisible = signal<boolean>(false);
   form = new FormGroup({
     username: new FormControl<string>('', {
       nonNullable: true,
